75deb0dbd3fc43634945af85778a49b1c3eea88f
Stella Preinitz Gärdinge <stella_pg@outlook.com>
2026-09-02 10:16:31 +0200

    Exercise 12 done (added InputMismatchException)

--- Practice/src/Exercise12.java
+++ Practice/src/Exercise12.java
@@ -1,11 +1,46 @@
+import java.util.Scanner;
+//InputMismatchException is a built-in tool to prevent illegal inputs.
+import java.util.InputMismatchException;
+
 public class Exercise12
 {
-// Skapa ett program som:
-// 1. Frågar vilket tal användaren vill ha tabellen för
-// 2. Skriver ut hela multiplikationstabellen för det talet (1-10)
-// Exempel: Om användaren skriver 7:
-// 7 x 1 = 7
-// 7 x 2 = 14
-// ... och så vidare
+    static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        //Variable delcared outside of loop to be used in the entire class
+        int validNumber = 0;
+        //Set to "false" to start loop
+        boolean isNumberValid = false;
 
+        System.out.println("Multiplication table");
+        System.out.println("====================");
+        System.out.println("Which table do you wish to see?");
+
+        //Loops until valid input is given
+        while (!isNumberValid)
+        {
+            System.out.println("Please input an integer: ");
+
+            //Accepts integer as input and flips bool to true to escape loop
+            try
+            {
+                validNumber = scanner.nextInt();
+                isNumberValid = true;
+            }
+            //Catches any input that is not integer, loop continues
+            catch (InputMismatchException Error)
+            {
+                System.out.println("Error: Not a valid integer.");
+                //clear buffer
+                scanner.next();
+            }
+        }
+
+        System.out.println("Presenting multiplication table for " + validNumber);
+        //Run loop from 1 to 10 to create standard table
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.println(validNumber * i);
+        }
+    }
 }
